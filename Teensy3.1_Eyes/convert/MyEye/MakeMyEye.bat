b7186047fbbc03cb9d9290ce2c0ce454f9943523
merlins-ghost <pyreates@hotmail.com>
2015-12-30 14:22:48 -0800

    UncannyEyes for Teensy 3.1/3.2
    Phillip Burgess' Teensy UncannyEyes showcased at
    https://learn.adafruit.com/animated-electronic-eyes-using-teensy-3-1
    Public repository dedicated to UncannyEyes .png and/or .h eye image
    files.
    Phillip Burgess' 'Uncanny Eyes' were inspired by a concept by David
    Boccabella (Marcwolf) on the Stan Winston School of Character Arts
    forums.
    Uncanny Eyes are PJRC Teensy 3.1/3.2 microcontroller controlled.
    Eyes are made from 4 or 5 PNG graphics files composed of an; upper
    eyelid, lower eyelid, sclera, iris, and an optional pupilmap.
    adafruit forum at
    https://forums.adafruit.com/viewtopic.php?f=22&t=81236&hilit=uncanny+eyes

---
+++ Teensy3.1_Eyes/convert/MyEye/MakeMyEye.bat
@@ -0,0 +1,5 @@
+python ..\tablegen.py scleraNone.png irisBlueFlames2.png upper.png lower.png -160 PupilMapDragon.png > ..\..\uncannyEyes\MyEye.h
+@pause
+"C:\Program Files (x86)\Arduino\arduino.exe" "%cd%\..\..\uncannyEyes\uncannyEyes.ino"
+
+
